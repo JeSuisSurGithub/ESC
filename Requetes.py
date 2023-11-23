9f72cf9f8065e61e1b4d287d4633b6658665542d
Goudoul <150365555+Goudoul@users.noreply.github.com>
2023-11-23 17:13:30 +0100

    Update Requetes.py

--- Requetes.py
+++ Requetes.py
@@ -31,8 +31,45 @@ def verifier_mdp(mdp,email):
     conn.commit()
     return("mdp verifie")
 
+# Exécution de la requête SELECT
+def obtenir_livre():
+    try:
+        cursor.execute('''SELECT id, titre, genre, date_de_parution FROM LIVRE''')
+        # Récupération des résultats sous forme de dictionnaires
+        results = []
+        for row in cursor.fetchall():
+            # Création d'un dictionnaire pour chaque ligne de résultat
+            result_dict = {
+                'id': row[0]
+                'titre': row[1],
+                'genre': row[2],
+                'date_de_parution': row[3],
+                }
+            results.append(result_dict)
+    except:
+        return("echecobtentionlivre")
+    return(results)
 
 
+def livre_emprunte(id_u):
+    try:
+        cursor.execute('''SELECT id, titre, genre, date_parution,date_debut, date_fin
+        FROM LIVRE JOIN EMPRUNT ON LIVRE.id==EMPRUNT.id_l WHERE id_u==?''',(id_u))
+        results = []
+        for row in cursor.fetchall():
+            # Création d'un dictionnaire pour chaque ligne de résultat
+            result_dict = {
+                'id': row[0]
+                'titre': row[1],
+                'genre': row[2],
+                'date_de_parution': row[3],
+                'date_debut': row[4],
+                'date_fin': row[5],
+                }
+            results.append(result_dict)
+    except:
+        return("livreemprunteechoue")
+    return(results)
 
 
 
