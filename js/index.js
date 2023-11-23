ca0b4ec711d2a020665802dc3fa76532c0156a9d
JecrisDesProgrammes <jutilisegougeul@gmail.com>
2023-11-23 16:14:23 +0100

    finition systeèmes de compte frontend

--- js/index.js
+++ js/index.js
@@ -1,3 +1,19 @@
+function est_un_email(email)
+{
+    const regex_email = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex_email.test(email);
+}
+
+function aujourdhui_yyyy_mm_dd()
+{
+    const aujourdhui = new Date();
+    const annee = aujourdhui.getFullYear();
+    const mois = String(aujourdhui.getMonth() + 1).padStart(2, '0');
+    const jour = String(aujourdhui.getDate()).padStart(2, '0');
+    const yyyy_mm_dd = `${annee}-${mois}-${jour}`;
+    return yyyy_mm_dd;
+}
+
 function est_connecte() {
     return new Promise((resolve, reject) => {
         fetch('/sql_statut')
@@ -12,11 +28,157 @@ function est_connecte() {
     });
 }
 
-est_connecte()
-    .then(info_statut => {
-        let msg_connect = info_statut.resultat ? "Connecté" : "Pas connecté";
-        document.getElementById("info_statut").innerHTML = `<p>${msg_connect}, Grade: ${info_statut.grade}</p><br/>`;
-    })
-    .catch(error => {
-        document.getElementById("info_statut").innerHTML = `<p>Statut de connexion indisponible (${error.message})</p><br/>`;
+async function connexion()
+{
+    var tag_email = document.getElementById("email");
+    var tag_motdepasse = document.getElementById("motdepasse");
+    var tag_info = document.getElementById("info_erreurs");
+
+    tag_info.innerHTML = "";
+    if (!est_un_email(tag_email.value)) {
+        tag_info.innerHTML += "<p>L'email n'est pas valide</p>";
+    }
+    if (tag_motdepasse.value.length < 4) {
+        tag_info.innerHTML += "<p>Le mot de passe doit faire au moins de 4 caractères</p>";
+    }
+    if (tag_info.innerHTML != "") return;
+
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (this.readyState != 4) return;
+        if (this.status == 200)
+        {
+            var reponse = JSON.parse(this.responseText);
+            tag_info.innerHTML = reponse.resultat ? "La connexion a réussi" : "La connexion a échoué";
+        }
+    };
+
+    xhr.open('POST', "/sql_connexion", true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify(
+        {
+            email: email.value,
+            motdepasse: motdepasse.value,
+        }
+    ));
+}
+
+async function deconnexion(silence = false)
+{
+    var tag_info = document.getElementById("info_erreurs");
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (this.readyState != 4) return;
+        if (this.status == 200)
+        {
+            var reponse = JSON.parse(this.responseText);
+            if (silence === false) {
+                tag_info.innerHTML = reponse.resultat ? "La déconnexion a réussi" : "La déconnexion a échoué";
+            }
+        }
+    };
+
+    xhr.open('POST', "/sql_deconnexion", true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send();
+}
+
+async function sinscrire()
+{
+    var tag_email = document.getElementById("email");
+    var tag_motdepasse = document.getElementById("motdepasse");
+    var tag_nom = document.getElementById("nom");
+    var tag_prenom = document.getElementById("prenom");
+    var tag_naissance = document.getElementById("naissance");
+    var tag_info = document.getElementById("info_erreurs");
+
+    tag_info.innerHTML = "";
+    if (!est_un_email(tag_email.value)) {
+        tag_info.innerHTML += "<p>L'email n'est pas valide</p>";
+    }
+    if (tag_motdepasse.value.length < 4) {
+        tag_info.innerHTML += "<p>Le mot de passe doit faire au moins de 4 caractères</p>";
+    }
+    if (tag_nom.value.length < 1) {
+        tag_info.innerHTML += "<p>Vous devez spécifier un nom</p>";
+    }
+    if (tag_prenom.value.length < 1) {
+        tag_info.innerHTML += "<p>Vous devez spécifier un prénom</p>";
+    }
+    if (tag_naissance.value == "" || aujourdhui_yyyy_mm_dd() <= tag_naissance.value) {
+        tag_info.innerHTML += "<p>Date invalide</p>";
+    }
+    if (tag_info.innerHTML != "") return;
+
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (this.readyState != 4) return;
+        if (this.status == 200)
+        {
+            var reponse = JSON.parse(this.responseText);
+            tag_info.innerHTML = reponse.resultat ? "L'inscription a réussi" : "L'inscription a échoué";
+        }
+    };
+
+    xhr.open('POST', "/sql_inscription", true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify(
+        {
+            email: tag_email.value,
+            motdepasse: tag_motdepasse.value,
+            nom: tag_nom.value,
+            prenom: tag_prenom.value,
+            naissance: tag_naissance.value
+        }
+    ));
+}
+
+async function supprimer()
+{
+    var tag_email = document.getElementById("email");
+    var tag_motdepasse = document.getElementById("motdepasse");
+    var tag_info = document.getElementById("info_erreurs");
+
+    tag_info.innerHTML = "";
+    if (!est_un_email(tag_email.value)) {
+        tag_info.innerHTML += "<p>L'email n'est pas valide</p>";
+    }
+    if (tag_motdepasse.value.length < 4) {
+        tag_info.innerHTML += "<p>Le mot de passe doit faire au moins de 4 caractères</p>";
+    }
+    if (tag_info.innerHTML != "") return;
+
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (this.readyState != 4) return;
+        if (this.status == 200)
+        {
+            var reponse = JSON.parse(this.responseText);
+            deconnexion({silence: true});
+            tag_info.innerHTML += reponse.resultat ? "La suppression a réussi" : "La suppression a échoué";
+        }
+    };
+
+    xhr.open('POST', "/sql_desinscription", true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify(
+        {
+            email: email.value,
+            motdepasse: motdepasse.value,
+        }
+    ));
+}
+
+function liste_livres(empruntes = false) {
+    return new Promise((resolve, reject) => {
+        fetch(empruntes ? '/sql_livres_empruntes' : '/sql_livres')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Request failed');
+                }
+                return response.json();
+            })
+            .then(data => resolve(data))
+            .catch(error => reject(error));
     });
+}
--- main.py
+++ main.py
@@ -17,6 +17,18 @@ class JSONConnexion(BaseModel):
     email: str
     motdepasse: str
 
+class JSONLivres(BaseModel):
+    titres: list[str]
+    genres: list[str]
+    date_parution: list[str]
+
+class JSONEmprunts(BaseModel):
+    titres: list[str]
+    genres: list[str]
+    date_parution: list[str]
+    date_emprunt_debut: list[str]
+    date_emprunt_fin: list[str]
+
 app = FastAPI()
 
 app.mount("/css", StaticFiles(directory="css"), name="css")
@@ -32,6 +44,12 @@ def racine() -> str:
 @app.get("/sql_statut")
 async def sql_statut():
     return {"resultat": EST_CONNECTE, "grade": 0}
+
+@app.post("/sql_deconnexion")
+async def sql_deconnexion():
+    global EST_CONNECTE
+    EST_CONNECTE = False
+    return {"resultat": True}
 
 @app.post("/sql_inscription")
 async def sql_inscription(user: JSONInscription):
@@ -53,6 +71,24 @@ async def sql_connexion(user: JSONConnexion):
     print(email, motdepasse)
     EST_CONNECTE = True
     return {"resultat": True}
+
+@app.post("/sql_desinscription")
+async def sql_desinscription(user: JSONConnexion):
+    global EST_CONNECTE
+    email = user.email
+    motdepasse = user.motdepasse
+    print(email, motdepasse)
+    EST_CONNECTE = False
+    return {"resultat": True}
+
+@app.get("/sql_livres")
+async def sql_statut():
+    return {"titres": ["bible", "coran"], "genres": ["religion", "religion"], "date_parution": ["????", "????"]}
+
+@app.get("/sql_livres_empruntes")
+async def sql_statut():
+    return {"titres": ["bible", "coran"], "genres": ["religion", "religion"], "date_parution": ["????", "????"], \
+"date_emprunt_debut": ["2023-06-11", "2023-08-11"], "date_emprunt_fin": ["2023-06-25", "2023-08-25"]}
 
 if __name__ == "__main__":
     uvicorn.run(app, port=8080, host='0.0.0.0')
