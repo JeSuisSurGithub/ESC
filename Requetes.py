dc0e6658450452ad37b5116f3093a436f5e767b8
Goudoul <150365555+Goudoul@users.noreply.github.com>
2023-11-23 16:19:46 +0100

    Create Requetes.py

---
+++ Requetes.py
@@ -0,0 +1,62 @@
+def ajouter_compte(mot_de_passe, nom, prenom, grade, email, date_de_naissance):
+    try:
+        cursor.execute('''INSERT INTO utilisateur (mot_de_passe, nom, prenom, grade, email, date_de_naissance)
+                        VALUES (?, ?, ?, ?, ?, ?)''',
+                    (mot_de_passe, nom, prenom, grade, email, date_de_naissance))
+    except:
+        return ("echoueajoutecompte")
+
+    conn.commit()
+    return("Compte ajouté avec succès.")
+
+def supprimer_compte(id):
+    try:
+        cursor.execute('''DELETE FROM utilisateur
+         WHERE id=?''',
+         (id))
+    except:
+        return("echouesupprimecompte")
+    
+    conn.commit()
+    return("Compte supprimer avec succès.")
+
+
+def verifier_mdp(mdp,email):
+    try:
+        cursor.execute('''SELECT mdp FROM utilisateur 
+        WHERE  )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
