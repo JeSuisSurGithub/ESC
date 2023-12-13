28c979717f2af51e676689dfddec0f8ca8f38426
Wa2si <128939596+Wa2si@users.noreply.github.com>
2023-12-13 18:06:01 +0100

    Update nfc.py

--- nfc.py
+++ nfc.py
@@ -1,23 +1,53 @@
-import time
-import nfc
+import RPi.GPIO as GPIO
+import MFRC522
+import signal
 
-# Créez une instance de l'objet ContactlessFrontend pour le lecteur NFC I2C
-clf = nfc.ContactlessFrontend('tty:AMA0:pn532')
+def lire_uid_nfc():
+    continue_reading = True
 
-def read_nfc():
-    print("Approchez un tag NFC...")
-    try:
-        tag = clf.connect(rdwr={'on-connect': lambda tag: False})
-        print("Tag NFC lu :")
-        print(tag)
-    except Exception as e:
-        print(f"Erreur lors de la lecture du tag NFC : {e}")
+    def end_read(signal, frame):
+        nonlocal continue_reading
+        print("Lecture terminée")
+        continue_reading = False
+        GPIO.cleanup()
 
-try:
-    while True:
-        read_nfc()
-        time.sleep(1)
-except KeyboardInterrupt:
-    pass
-finally:
-    clf.close()
+    signal.signal(signal.SIGINT, end_read)
+    MIFAREReader = MFRC522.MFRC522()
+
+    print("Passer le tag RFID à lire")
+
+    while continue_reading:
+
+        # Detecter les tags
+        (status, TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+
+        # Une carte est detectee
+        if status == MIFAREReader.MI_OK:
+            print("Carte détectée")
+
+            # Recuperation UID
+            (status, uid) = MIFAREReader.MFRC522_Anticoll()
+
+            if status == MIFAREReader.MI_OK:
+                print("UID de la carte : " + ".".join(map(str, uid)))
+                return uid
+
+                # Clee d'authentification par defaut
+                key = [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]
+
+                # Selection du tag
+                MIFAREReader.MFRC522_SelectTag(uid)
+
+                # Authentification
+                status = MIFAREReader.MFRC522_Auth(MIFAREReader.PICC_AUTHENT1A, 8, key, uid)
+
+                if status == MIFAREReader.MI_OK:
+                    MIFAREReader.MFRC522_Read(8)
+                    MIFAREReader.MFRC522_StopCrypto1()
+                else:
+                    print("Erreur d'Authentification")
+
+# Utilisez la fonction pour lire l'UID
+uid_carte_nfc = lire_uid_nfc()
+print("UID de la carte NFC détectée :", ".".join(map(str, uid_carte_nfc)))
+
