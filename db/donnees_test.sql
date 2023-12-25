1e33aa2c81020e64a548ac901cc5b2b194efb7f7
JutiliseGit <jutilisegougeul@gmail.com>
2023-12-25 18:53:04 +0100

    maj donnees test

--- db/donnees_test.sql
+++ db/donnees_test.sql
@@ -12,15 +12,3 @@ INSERT INTO LIVRE (titre, genre, rayon, date_parution, guid_nfc)
 
 INSERT INTO LIVRE (titre, genre, rayon, date_parution, guid_nfc)
     VALUES ('livre5', 'roman', '2E', '1650-03-06', '00-55AA-00');
-
-INSERT INTO EMPRUNT (id_u, id_l, date_debut, date_fin, rendu)
-    VALUES ('1', '1', '2023-11-24', '2023-12-01', 0);
-
-INSERT INTO EMPRUNT (id_u, id_l, date_debut, date_fin, rendu)
-    VALUES ('1', '3', '2023-12-01', '2023-12-12', 1);
-
-INSERT INTO EMPRUNT (id_u, id_l, date_debut, date_fin, rendu)
-    VALUES ('1', '4', '2023-12-15', '2023-12-24', 0);
-
-INSERT INTO EMPRUNT (id_u, id_l, date_debut, date_fin, rendu)
-    VALUES ('1', '5', '2023-12-18', '2023-12-20', 0);
