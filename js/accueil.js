e823014c7004a0fa87761fe735bb92420a52469e
JutiliseGit <jutilisegougeul@gmail.com>
2023-12-28 21:02:59 +0100

    bugfix recherche decoadage uri

--- js/accueil.js
+++ js/accueil.js
@@ -1,92 +1,3 @@
-// Recherche
-document.addEventListener("DOMContentLoaded", function() {
-    let searchBtn = document.querySelector('.searchBtn');
-    let closeBtn = document.querySelector('.closeBtn');
-    let searchBox = document.querySelector('.searchBox');
-    let inputField = document.getElementById('valeurs');
-
-    searchBtn.onclick = function() {
-        if (searchBtn.classList.contains('active') && inputField.value.trim() !== '') {
-            const url = construire_requete_get(`${window.location.origin}/html/recherche.html`, {
-                entree: encodeURIComponent(inputField.value)
-            });
-            window.location.href = url;
-        }
-
-        searchBox.classList.add('active');
-        closeBtn.classList.add('active');
-        searchBtn.classList.add('active');
-    }
-
-    closeBtn.onclick = function() {
-        searchBox.classList.remove('active');
-        closeBtn.classList.remove('active');
-        searchBtn.classList.remove('active');
-    }
-});
-
-// Actions
-async function deconnexion() {
-    await api_deconnexion();
-    window.location.href = window.location.origin;
-}
-
-async function emprunter() {
-    const redirection = construire_requete_get(`${window.location.origin}/html/nfc.html`, {
-        provenance: `${encodeURIComponent(window.location.href.split("?")[0])}`,
-        action: "emprunt",
-    });
-    window.location.href = redirection;
-}
-
-async function retour() {
-    const redirection = construire_requete_get(`${window.location.origin}/html/nfc.html`, {
-        provenance: `${encodeURIComponent(window.location.href.split("?")[0])}`,
-        action: "retour",
-    });
-    window.location.href = redirection;
-}
-
-async function verifier_action() {
-    const code = parametre_get("code");
-    if (code > 0) {
-        const info_conn = await api_statut();
-        if (info_conn.code > 0) {
-            const requete_livre = await api_livres();
-            if (requete_livre.code > 0) {
-                const index = requete_livre.val.uid_nfc.indexOf(parametre_get("uid"))
-
-                if (parametre_get("action") == "emprunt") {
-                    if (index !== -1) {
-                        const resultat = await api_emprunt(requete_livre.val.id[index]);
-                        window.alert(G_CODE_ERREURS[resultat.code]);
-                        window.location.href = window.location.href.split("?")[0];
-                    } else {
-                        window.alert("Carte de livre inconnue");
-                    }
-                } else if (parametre_get("action") == "retour") {
-                    if (index !== -1) {
-                        const resultat = await api_retour(requete_livre.val.id[index]);
-                        window.alert(G_CODE_ERREURS[resultat.code]);
-                        window.location.href = window.location.href.split("?")[0];
-                    } else {
-                        window.alert("Carte de livre inconnue");
-                    }
-                } else {
-                    window.alert("Action impossible")
-                }
-            }
-            else {
-                window.alert(G_CODE_ERREURS[requete_livre.code]);
-            }
-        } else {
-            window.alert(G_CODE_ERREURS[info_conn.code]);
-        }
-    } else if (code !== null) {
-        window.alert(G_CODE_ERREURS[code]);
-    }
-}
-
 // Liste emprunts
 (async () => {
     const sortie = document.getElementById("liste_emprunts")
@@ -124,7 +35,6 @@ async function verifier_action() {
         window.alert(G_CODE_ERREURS[livres_empruntes.code]);
     }
 })();
-verifier_action();
 
 // Calendrier
 const header = document.querySelector(".calendar h3");
--- js/recherche.js
+++ js/recherche.js
@@ -1,90 +1,3 @@
-// Recherche
-document.addEventListener("DOMContentLoaded", function() {
-    let searchBtn = document.querySelector('.searchBtn');
-    let closeBtn = document.querySelector('.closeBtn');
-    let searchBox = document.querySelector('.searchBox');
-    let inputField = document.getElementById('valeurs');
-
-    searchBtn.onclick = function() {
-        if (searchBtn.classList.contains('active') && inputField.value.trim() !== '') {
-            const url = construire_requete_get(`${window.location.origin}/html/recherche.html`, {
-                entree: encodeURIComponent(inputField.value)
-            });
-            window.location.href = url;
-        }
-
-        searchBox.classList.add('active');
-        closeBtn.classList.add('active');
-        searchBtn.classList.add('active');
-    }
-
-    closeBtn.onclick = function() {
-        searchBox.classList.remove('active');
-        closeBtn.classList.remove('active');
-        searchBtn.classList.remove('active');
-    }
-});
-
-// Actions
-async function deconnexion() {
-    await api_deconnexion();
-    window.location.href = window.location.origin;
-}
-
-async function emprunter() {
-    const redirection = construire_requete_get(`${window.location.origin}/html/nfc.html`, {
-        provenance: `${encodeURIComponent(window.location.href.split("?")[0])}`,
-        action: "emprunt",
-    });
-    window.location.href = redirection;
-}
-
-async function retour() {
-    const redirection = construire_requete_get(`${window.location.origin}/html/nfc.html`, {
-        provenance: `${encodeURIComponent(window.location.href.split("?")[0])}`,
-        action: "retour",
-    });
-    window.location.href = redirection;
-}
-
-async function verifier_action() {
-    const code = parametre_get("code");
-    if (code > 0) {
-        const info_conn = await api_statut();
-        if (info_conn.code > 0) {
-            const requete_livre = await api_livres();
-            if (requete_livre.code > 0) {
-                const index = requete_livre.val.uid_nfc.indexOf(parametre_get("uid"))
-
-                if (parametre_get("action") == "emprunt") {
-                    if (index !== -1) {
-                        const resultat = await api_emprunt(requete_livre.val.id[index]);
-                        window.alert(G_CODE_ERREURS[resultat.code]);
-                    } else {
-                        window.alert("Carte de livre inconnue");
-                    }
-                } else if (parametre_get("action") == "retour") {
-                    if (index !== -1) {
-                        const resultat = await api_retour(requete_livre.val.id[index]);
-                        window.alert(G_CODE_ERREURS[resultat.code]);
-                    } else {
-                        window.alert("Carte de livre inconnue");
-                    }
-                } else {
-                    window.alert("Action impossible")
-                }
-            }
-            else {
-                window.alert(G_CODE_ERREURS[requete_livre.code]);
-            }
-        } else {
-            window.alert(G_CODE_ERREURS[info_conn.code]);
-        }
-    } else if (code !== null) {
-        window.alert(G_CODE_ERREURS[code]);
-    }
-}
-
 function recherche(entree, vtitre, vgenre) {
     const termes = entree.split(" ");
     let vid = []
@@ -115,7 +28,7 @@ function recherche(entree, vtitre, vgenre) {
     const sortie = document.getElementById("liste_resultats")
     const livres = await api_livres();
     if (livres.code > 0) {
-        const entree = parametre_get("entree");
+        const entree = decodeURIComponent(parametre_get("entree"));
         const indices = recherche(entree, livres.val.titre, livres.val.genre);
         if (indices.length === 0) {
             sortie.innerHTML = `
@@ -147,4 +60,3 @@ function recherche(entree, vtitre, vgenre) {
         window.alert(G_CODE_ERREURS[livres.code]);
     }
 })();
-verifier_action();
